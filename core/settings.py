cac08affae68207b4d00832ac909f1e0d2acda0f
Dave Hannemann <davidhannemanndev@gmail.com>
2026-05-21 17:38:02 +0200

    fix: Static root

--- core/settings.py
+++ core/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = BASE_DIR / "static"
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
