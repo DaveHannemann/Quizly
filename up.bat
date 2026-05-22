77f5bd286aed55521d3d5814efee1e91f3a6344b
Dave Hannemann <davidhannemanndev@gmail.com>
2026-05-22 19:33:23 +0200

    updated version

--- up.bat
+++ up.bat
@@ -5,4 +5,4 @@ set /p msg=Commit message:
 
 git commit -m "%msg%"
 git push
-ssh davidhannemanndev@34.105.215.66 "cd /home/davidhannemanndev/projects/Quizly && git fetch origin && git reset --hard origin/main && sudo supervisorctl restart quizly_gunicorn"
+ssh -t davidhannemanndev@34.105.215.66 "cd /home/davidhannemanndev/projects/Quizly && git fetch origin && git reset --hard origin/main && sudo supervisorctl restart quizly_gunicorn"
